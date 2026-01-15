64a3f003b25af19a0b7539010b9afe55480fa267
Daniel Mouris <dgmouris@gmail.com>
2026-01-15 09:26:16 -0700

    class progress pushed

---
+++ 3-intro-blog-example-continued-start/myblogwebsite/blog/urls.py
@@ -0,0 +1,8 @@
+# this is going to map the urls in the
+# browser to the views itself.
+# this is how we connect urls to functionality
+from django.urls import path
+# import all of the views
+from . import views
+# the line above is importing all functions
+# from views in the current folder
