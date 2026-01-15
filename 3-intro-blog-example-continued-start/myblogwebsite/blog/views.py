3e4e40eb37bd5c2cdb601d9c70f77a82b5214440
Daniel Mouris <dgmouris@gmail.com>
2026-01-15 09:24:35 -0700

    class progress pushed

--- 3-intro-blog-example-continued-start/myblogwebsite/blog/views.py
+++ 3-intro-blog-example-continued-start/myblogwebsite/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Render is a special function in django
+# that we'll learn more about that essentially
+# returns a template as a response
+
+def home(request):
+
+    # the line below is going
+    # to be what's passed as the response
+    return render(
+        request,
+        'blog/posts_list.html'
+    )
+    # this is going to look in the templates
+    # folder (more on this later.)
