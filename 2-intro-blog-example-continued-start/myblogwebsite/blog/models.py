2c2a47fcce63ef0f5142756783efc92862fedb33
Daniel Mouris <dgmouris@gmail.com>
2026-01-15 08:43:21 -0700

    class progress pushed

--- 2-intro-blog-example-continued-start/myblogwebsite/blog/models.py
+++ 2-intro-blog-example-continued-start/myblogwebsite/blog/models.py
@@ -4,7 +4,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+# this class is going to be how we interact with
+# the database
+# 1. we create the model
+# 2. we need to apply this to the database so the first thing
+#    we do is python manage.py makemigrations which is going to
+#    create the file to apply this database table.
+# 3. we need to use python manage.py migrate to apply the table.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
