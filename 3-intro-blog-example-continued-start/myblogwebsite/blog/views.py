6f36feebfe31c0531facc04913943a122de4468a
Daniel Mouris <dgmouris@gmail.com>
2026-01-16 10:40:12 -0700

    class progress pushed

--- 3-intro-blog-example-continued-start/myblogwebsite/blog/views.py
+++ 3-intro-blog-example-continued-start/myblogwebsite/blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+# we're going import the model
+from .models import Post
+
 # Render is a special function in django
 # that we'll learn more about that essentially
 # returns a template as a response
 
 def post_list(request):
+    # one of the things you can do
+    breakpoint()
 
     # the line below is going
     # to be what's passed as the response
