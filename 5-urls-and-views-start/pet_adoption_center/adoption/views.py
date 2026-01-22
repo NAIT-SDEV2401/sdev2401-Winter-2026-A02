c9f52d9ddb7df0843c2a101f7c959803e9f24288
Daniel Mouris <dgmouris@gmail.com>
2026-01-22 09:07:10 -0700

    class progress pushed

--- 5-urls-and-views-start/pet_adoption_center/adoption/views.py
+++ 5-urls-and-views-start/pet_adoption_center/adoption/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+# this is going to handle requests
+# and give responses to those requests
+def home_page(request):
+    # for many views we're going to
+    # have some type of logic here
+    # that will do somethign with the
+    # request
+
+    # return a response.
+    return render(
+        request, # pass this to the response
+        "adoption/home_page.html" # the template
+    )
