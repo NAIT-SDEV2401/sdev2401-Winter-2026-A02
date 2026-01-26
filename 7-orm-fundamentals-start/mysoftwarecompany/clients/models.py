6f12113ab31d34023f24692e26dfb2e4fa17af07
Daniel Mouris <dgmouris@gmail.com>
2026-01-26 14:27:23 -0700

    class progress pushed

--- 7-orm-fundamentals-start/mysoftwarecompany/clients/models.py
+++ 7-orm-fundamentals-start/mysoftwarecompany/clients/models.py
@@ -5,9 +5,12 @@ from django.db import models
 class Company(models.Model):
     # below here is going to be fields in the database
     name = models.CharField(max_length=100)
-
+    # this goign to make a column in the table named
+    # name with max character length of 100
     email = models.EmailField(max_length=100, unique=True)
-
+    # this goign to make a column in the table named
+    # email with max character length of 100
+    # except it needs to unique
 
     def __str__(self):
         return self.name
