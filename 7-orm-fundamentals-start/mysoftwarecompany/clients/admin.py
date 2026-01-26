60dc8dd1c63c463c14d26b66d0f96c81b3b39ba7
Daniel Mouris <dgmouris@gmail.com>
2026-01-26 14:58:01 -0700

    class progress pushed

--- 7-orm-fundamentals-start/mysoftwarecompany/clients/admin.py
+++ 7-orm-fundamentals-start/mysoftwarecompany/clients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company
+
+# we're going to make the company model navigatable
+# in the admin.
+
+admin.site.register(Company)
+# this is going add our site to the admin
+# so that we can add and edit to our hearts
+# content.
