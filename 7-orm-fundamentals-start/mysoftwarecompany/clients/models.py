67e37565cf4f9ce1c7560a1a28536896adfca732
Daniel Mouris <dgmouris@gmail.com>
2026-01-26 14:26:09 -0700

    class progress pushed

--- 7-orm-fundamentals-start/mysoftwarecompany/clients/models.py
+++ 7-orm-fundamentals-start/mysoftwarecompany/clients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# classes that inherit from models.Model are going
+# to be what's in the database.
+class Company(models.Model):
+    # below here is going to be fields in the database
+    name = models.CharField(max_length=100)
+
+    email = models.EmailField(max_length=100, unique=True)
+
+
+    def __str__(self):
+        return self.name
