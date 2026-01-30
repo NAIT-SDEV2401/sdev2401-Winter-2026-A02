37b0783500fd3f7543bc552fb1eda0cfa62550b3
Daniel Mouris <dgmouris@gmail.com>
2026-01-30 10:43:10 -0700

    class progress pushed

--- 7-orm-fundamentals-start/mysoftwarecompany/exploring_database_1.py
+++ 7-orm-fundamentals-start/mysoftwarecompany/exploring_database_1.py
@@ -12,9 +12,18 @@ from clients.models import Company
 # use .objects. to interact with the items.
 # let's read all of the items here.
 
+print("Selecting all elements here.")
+# select all from the database
 companies = Company.objects.all()
 
 # let's take a look at the records.
 print(companies)
-breakpoint()
 
+print("Let's filter to select on a few (subset)")
+# select where from the database
+acme_company = Company.objects.filter(name="Acme Inc.")
+# Acme Inc. is just what I used you can use somethign different.
+
+print("Company filtered for acme inc")
+print(acme_company)
+breakpoint()
