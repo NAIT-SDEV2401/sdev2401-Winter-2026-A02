0bb3da8fc6cbbbed7be8a2d1b729c6544f722d02
Daniel Mouris <dgmouris@gmail.com>
2026-01-30 10:28:34 -0700

    class progress pushed

--- 7-orm-fundamentals-start/mysoftwarecompany/exploring_database_1.py
+++ 7-orm-fundamentals-start/mysoftwarecompany/exploring_database_1.py
@@ -7,3 +7,14 @@ print("Django environment set up successfully.")
 
 # We're going to import the model so that we
 # can interact with it.
+from clients.models import Company
+
+# use .objects. to interact with the items.
+# let's read all of the items here.
+
+companies = Company.objects.all()
+
+# let's take a look at the records.
+print(companies)
+breakpoint()
+
