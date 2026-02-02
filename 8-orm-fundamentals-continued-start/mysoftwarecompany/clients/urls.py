ab574bea2cb85b8daf65c8137fc58fac1d772ff4
Daniel Mouris <dgmouris@gmail.com>
2026-02-02 13:31:56 -0700

    class progress pushed

--- 8-orm-fundamentals-continued-start/mysoftwarecompany/clients/urls.py
+++ 8-orm-fundamentals-continued-start/mysoftwarecompany/clients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 # we're going use the view we've created.
+from .views import list_companies
 
 urlpatterns = [
-
+    path("", list_companies, name="company_list"),
 ]
