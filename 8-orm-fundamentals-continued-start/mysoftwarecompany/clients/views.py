8bcd7c79c1d41b59d55585bddfd3c8e1affa5d7b
Daniel Mouris <dgmouris@gmail.com>
2026-02-02 13:23:58 -0700

    class progress pushed

--- 8-orm-fundamentals-continued-start/mysoftwarecompany/clients/views.py
+++ 8-orm-fundamentals-continued-start/mysoftwarecompany/clients/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# import the model
+
+def list_companies(request):
+
+    # get all companies
+
+    return # a response
+        # request
+        # template name
+        # pass companies into the context
