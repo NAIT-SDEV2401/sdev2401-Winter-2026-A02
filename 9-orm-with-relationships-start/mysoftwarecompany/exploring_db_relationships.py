5bd7bd67569f9b907223c103c69576e10b812681
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 08:42:55 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships.py
@@ -5,6 +5,11 @@ django.setup()
 
 print("Django environment set up successfully.")
 
-# let's
 from clients.models import Employee, Company
 
+# first select acme inc. from company
+# we're going to use .get to select a single instance
+COMPANY_NAME = "Acme Inc."
+company = Company.objects.get(name=COMPANY_NAME)
+
+breakpoint()
