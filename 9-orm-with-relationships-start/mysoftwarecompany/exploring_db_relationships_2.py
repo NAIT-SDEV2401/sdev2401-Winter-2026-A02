4a67ecebb6e61fad7e583de2f3cdbf2327b0a14e
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 09:44:54 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_2.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_2.py
@@ -6,6 +6,7 @@ django.setup()
 print("Django environment set up successfully.")
 
 # import role
+from clients.models import Role
 
 roles_data = [
     {"name": "CEO", "description": "Chief Executive Officer"},
@@ -14,5 +15,14 @@ roles_data = [
 ]
 
 # I want you to loop through the dictionaries
+for role_data in roles_data:
+    role, created = Role.objects.get_or_create(
+        name=role_data['name'],
+        description=role_data['description'],
+    )
     # use get_or_create on the role
-    # display if it's created successfull
+    # display if it's created successfully
+    if created:
+        print("Created new role {role}")
+    else:
+        print("Selected role {role} from db")
