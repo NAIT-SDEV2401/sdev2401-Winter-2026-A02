4bb8d2b271b00585d827c51081b2ad800f959759
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 08:49:01 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships.py
@@ -10,10 +10,16 @@ from clients.models import Employee, Company
 # first select acme inc. from company
 # we're going to use .get to select a single instance
 COMPANY_NAME = "Acme Inc."
-company = Company.objects.get(name=COMPANY_NAME)
+acme_company = Company.objects.get(name=COMPANY_NAME)
 
 # you can get the employees for this company in
 # a couple of ways
-all_employees = company.employees.all()
+all_employees = acme_company.employees.all()
 # where "employees" the related_name to company
 # on the Employee model.
+
+# from the employee model
+all_employees = Employee.objects.filter(
+    company=acme_company
+)
+
