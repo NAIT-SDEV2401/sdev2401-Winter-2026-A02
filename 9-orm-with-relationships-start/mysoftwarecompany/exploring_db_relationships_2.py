1c14246aee123f211aa8ce0dfd381d2ea04fabf6
Daniel Mouris <dgmouris@gmail.com>
2026-02-05 09:37:30 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_2.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_2.py
@@ -5,8 +5,14 @@ django.setup()
 
 print("Django environment set up successfully.")
 
+# import role
+
 roles_data = [
     {"name": "CEO", "description": "Chief Executive Officer"},
     {"name": "Manager", "description": "Manages a team of employees"},
     {"name": "Developer", "description": "Writes code and develops software"}
 ]
+
+# I want you to loop through the dictionaries
+    # use get_or_create on the role
+    # display if it's created successfull
