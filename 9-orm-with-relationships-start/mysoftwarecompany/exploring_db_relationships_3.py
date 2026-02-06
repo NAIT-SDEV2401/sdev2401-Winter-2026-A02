2f4745812473438d3fa8d777ae6fa6005cc21483
Daniel Mouris <dgmouris@gmail.com>
2026-02-06 11:03:44 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_3.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_3.py
@@ -11,11 +11,22 @@ from clients.models import Employee, Role
 # select acme employees
 # where company name is Acme Inc.
 # Get the first employee
-acme_employees = Employee.objects.filter(
+first_acme_employee = Employee.objects.filter(
     company__name="Acme Inc."
-)
-breakpoint()
+).first() # .first just selects the first one.
 
 # set that employee's role to CEO if it's
 # not defined
 # you need to get those Roles
+ceo_role = Role.objects.filter(name="CEO").first()
+# is the same as below.
+# ceo_role = Role.objects.get(name="CEO")
+
+if not first_acme_employee.role:
+    # example of update.
+    first_acme_employee.role = ceo_role
+    first_acme_employee.save()
+    print("saved.")
+
+print(F"the role of {first_acme_employee} is {first_acme_employee.role}")
+
