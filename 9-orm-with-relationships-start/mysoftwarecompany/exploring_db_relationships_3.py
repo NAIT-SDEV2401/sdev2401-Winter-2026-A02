d9639b3006fcd51d918d6d4b733713a38db4a16c
Daniel Mouris <dgmouris@gmail.com>
2026-02-06 10:44:38 -0700

    class progress pushed

--- 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_3.py
+++ 9-orm-with-relationships-start/mysoftwarecompany/exploring_db_relationships_3.py
@@ -6,10 +6,15 @@ django.setup()
 print("Django environment set up successfully.")
 
 # import Role Employee
+from clients.models import Employee, Role
 
 # select acme employees
 # where company name is Acme Inc.
 # Get the first employee
+acme_employees = Employee.objects.filter(
+    company__name="Acme Inc."
+)
+breakpoint()
 
 # set that employee's role to CEO if it's
 # not defined
