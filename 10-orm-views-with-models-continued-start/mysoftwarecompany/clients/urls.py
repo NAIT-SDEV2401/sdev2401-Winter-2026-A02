c45ad3269624f715bfb18003192849226a5674f8
Daniel Mouris <dgmouris@gmail.com>
2026-02-09 13:39:57 -0700

    class progress pushed

--- 10-orm-views-with-models-continued-start/mysoftwarecompany/clients/urls.py
+++ 10-orm-views-with-models-continued-start/mysoftwarecompany/clients/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import list_companies
+from .views import list_companies, company_detail
 
 urlpatterns = [
     path('companies/', list_companies, name='companies_list'),
+    # let's create a company detail page.
+    path('company/<int:company_id>/', company_detail, name="company_detail")
 ]
--- 10-orm-views-with-models-continued-start/mysoftwarecompany/clients/views.py
+++ 10-orm-views-with-models-continued-start/mysoftwarecompany/clients/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Company
@@ -9,3 +9,12 @@ def list_companies(request):
     companies = Company.objects.all()
 
     return render(request, 'clients/companies_list.html', {'companies': companies})
+
+# I want to create a detail company view.
+def company_detail(request, company_id):
+    # we're goign to use the company id.
+
+    return render(
+        request,
+        'clients/company_detail.html',
+    )
