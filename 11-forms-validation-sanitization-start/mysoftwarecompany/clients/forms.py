83cbe540cb86872bc37b8a0817a2b86d5a24c49a
Daniel Mouris <dgmouris@gmail.com>
2026-02-23 13:35:32 -0700

    class progress pushed

--- 11-forms-validation-sanitization-start/mysoftwarecompany/clients/forms.py
+++ 11-forms-validation-sanitization-start/mysoftwarecompany/clients/forms.py
@@ -9,3 +9,17 @@ class ContactForm(forms.Form):
     message = forms.CharField(
         widget=forms.Textarea, required=True
     )
+
+    # is we're going to perform a little more validation.
+    # check to see if the name is greater than 2 characters.
+    def clean_name(self):
+        # get the cleaned name here (from the cleaned_data dict)
+        name = self.cleaned_data.get('name')
+
+        if len(name) < 2:
+            # if it's lessa than two we're going to raise a validation error.
+            raise forms.ValidationError(
+                "name must be greater than 2 characters"
+            )
+        # otherwise return the name.
+        return name
