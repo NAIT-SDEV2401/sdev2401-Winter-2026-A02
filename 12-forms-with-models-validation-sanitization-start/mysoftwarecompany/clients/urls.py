7ddd6977c954bc30292f7043759e572a8885d332
Daniel Mouris <dgmouris@gmail.com>
2026-02-23 14:22:15 -0700

    class progress pushed

--- 12-forms-with-models-validation-sanitization-start/mysoftwarecompany/clients/urls.py
+++ 12-forms-with-models-validation-sanitization-start/mysoftwarecompany/clients/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-    list_companies, company_detail, employees_search_results, contact_us
+    list_companies, company_detail, employees_search_results,
+    contact_us, create_company
 )
 
 urlpatterns = [
+    path('company/create/', create_company, name="company_create"),
     path('companies/', list_companies, name='companies_list'),
     path('company/<int:company_id>/', company_detail, name='company_detail'),
     path('company/<int:company_id>/employees/results/', employees_search_results, name='employees_search_results'),
