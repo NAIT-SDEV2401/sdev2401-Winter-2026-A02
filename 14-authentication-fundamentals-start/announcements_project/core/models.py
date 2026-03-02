baf9a7d188fda2c23a3601b91cbdd2e4b5c35cec
Daniel Mouris <dgmouris@gmail.com>
2026-03-02 14:26:06 -0700

    class progress pushed

--- 14-authentication-fundamentals-start/announcements_project/core/models.py
+++ 14-authentication-fundamentals-start/announcements_project/core/models.py
@@ -5,12 +5,18 @@ from django.contrib.auth.models import AbstractUser
 # django provides a user by default here but we're going to customize it.
 
 class User(AbstractUser):
+    # a tuple is an list that can't be modified
+    # treat it as a constant (why it's capitals)
     ROLE_CHOICES = (
+        # value on the left, what's display on the right
         ('teacher', 'Teacher'),
         ('student', 'Student')
     )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES)
+    # passing the role choices makes it a drop down
     def __str__(self):
         # Note that username is from the abstract base user.
         return F"{self.username} ({self.role})"
