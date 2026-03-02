b770d1cf97f9ec092d6891c1f950e3ad92a835f3
Daniel Mouris <dgmouris@gmail.com>
2026-03-02 14:12:43 -0700

    class progress pushed

---
+++ 14-authentication-fundamentals-start/announcements_project/core/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# import the default user that we're going to override.
+from django.contrib.auth.models import AbstractUser
+# django provides a user by default here but we're going to customize it.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('teacher', 'Teacher'),
+        ('student', 'Student')
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        # Note that username is from the abstract base user.
+        return F"{self.username} ({self.role})"
