6b4ba72ae34131c5195dfa4f64f88d9890252001
Daniel Mouris <dgmouris@gmail.com>
2026-03-02 14:48:03 -0700

    class progress pushed

--- 14-authentication-fundamentals-start/announcements_project/announcements/urls.py
+++ 14-authentication-fundamentals-start/announcements_project/announcements/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import announcement_list, create_announcement
 
 urlpatterns = [
+    # we're using this over here.
     path('', announcement_list, name='announcement_list'),
     path('create/', create_announcement, name='create_announcement'),
 ]
