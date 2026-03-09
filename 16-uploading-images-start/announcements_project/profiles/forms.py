6a66d161df1482479e34b7e80f4cd9bf153e8af9
Daniel Mouris <dgmouris@gmail.com>
2026-03-09 14:33:01 -0600

    class progress pushed

---
+++ 16-uploading-images-start/announcements_project/profiles/forms.py
@@ -0,0 +1,10 @@
+# we're going to build on our modelform
+# and the idea that we'll upload the user
+# in the view based on request.user.
+from django import forms
+from .models import Profile
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'profile_picture']
