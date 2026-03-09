11bf5227a969f6a6398e6b5c8883be182b6e0077
Daniel Mouris <dgmouris@gmail.com>
2026-03-09 14:38:10 -0600

    class progress pushed

--- 16-uploading-images-start/announcements_project/profiles/views.py
+++ 16-uploading-images-start/announcements_project/profiles/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.decorators import login_required
+
 from django.shortcuts import render
 
+# import the model
+from .models import Profile
+
 # Create your views here.
+@login_required
+def update_profile(request):
+    # we're going to get or create a profile from the db.
+    profile, created = Profile.objects.get_or_create(
+        user=request.user
+    )
+
+
