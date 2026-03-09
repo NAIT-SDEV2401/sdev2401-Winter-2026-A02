fcaf4d7d53b19c8dad27291f59d87037b2ce00d0
Daniel Mouris <dgmouris@gmail.com>
2026-03-09 14:21:53 -0600

    class progress pushed

--- 16-uploading-images-start/announcements_project/profiles/models.py
+++ 16-uploading-images-start/announcements_project/profiles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    # map a single user to a single profile.
+    # it's like a foreignkey but more strict to only map one to one.
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    bio = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        # looks at the username of the user model.
+        return F"Profile of {self.user.username}"
