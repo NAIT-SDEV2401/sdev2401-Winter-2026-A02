ca7f792965dfff8f9ed5d338fa7dcf93561ac3b5
Daniel Mouris <dgmouris@gmail.com>
2026-03-12 09:06:35 -0600

    class progress pushed

--- 16-uploading-images-start/announcements_project/profiles/urls.py
+++ 16-uploading-images-start/announcements_project/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import update_profile
+from .views import update_profile, profile_list
 
 urlpatterns = [
     path("edit/", update_profile, name="profile_edit"),
+    path("", profile_list, name="profile_list"),
 ]
--- 16-uploading-images-start/announcements_project/profiles/views.py
+++ 16-uploading-images-start/announcements_project/profiles/views.py
@@ -8,6 +8,24 @@ from .models import Profile
 # import form
 from .forms import ProfileForm
 
+# create the list of the profiles.
+# url
+# view
+# fetch the profiles
+# create the template with the url of the image.
+# use the html provided.
+
+
+@login_required
+def profile_list(request):
+    profiles = Profile.objects.all()
+
+    return render(
+        request,
+        "profiles/profile_list.html",
+        {"profiles": profiles},
+    )
+
 
 # Create your views here.
 @login_required
@@ -15,7 +33,6 @@ def update_profile(request):
     # we're going to get or create a profile from the db.
     profile, created = Profile.objects.get_or_create(user=request.user)
     if request.method == "POST":
-        breakpoint()
         form = ProfileForm(
             request.POST,
             request.FILES,  # the images that are passed back.
