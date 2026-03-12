6b376deccdc2fab173e426f81adfe0de6080124b
Daniel Mouris <dgmouris@gmail.com>
2026-03-12 09:42:39 -0600

    class progress pushed

--- 17-uploading-non-image-files-start/announcements_project/courses/views.py
+++ 17-uploading-non-image-files-start/announcements_project/courses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+# create our bulk assingment upload view
+
+# create the view
+@login_required
+def bulk_assignment_upload(request)
+# that uses the form that we just created
+# the get file from the cleaned data
