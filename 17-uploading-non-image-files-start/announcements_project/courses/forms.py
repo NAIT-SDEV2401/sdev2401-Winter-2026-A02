377873590dcc07aa5a78e3139ecf2eae0457517d
Daniel Mouris <dgmouris@gmail.com>
2026-03-12 09:31:50 -0600

    class progress pushed

---
+++ 17-uploading-non-image-files-start/announcements_project/courses/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+
+# this is going to take a file that's a csv
+# ensure that it's a csv in our validation
+# in our model we're going to import all of the items.
+class BulkAssignmentUploadForm(forms.Model):
+    csv_file = forms.FileField(
+        label="Select a CSV",
+    )
+
+    # Let's add some validation on this file.
+    def clean_csv_file(self):
+        file = self.cleaned_data.get("csv_file")
+        # check a few properties of the file.
+        # like the file type extension.
+        if not file.name.endswith(".csv"):
+            raise forms.ValidationError(
+                "Please upload a CSV file",
+            )
+
+        # check the content type of the file.
+        if file.content_type != "text/csv":
+            raise forms.ValidationError("File type is not CSV.")
+
+        return file
