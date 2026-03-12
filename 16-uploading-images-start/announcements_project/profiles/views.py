47ed86b6b5fff3dea616b909456f97c7a2208ec6
Daniel Mouris <dgmouris@gmail.com>
2026-03-12 08:18:09 -0600

    class progress pushed

--- 16-uploading-images-start/announcements_project/profiles/views.py
+++ 16-uploading-images-start/announcements_project/profiles/views.py
@@ -4,34 +4,31 @@ from django.shortcuts import render, redirect
 
 # import the model
 from .models import Profile
+
 # import form
 from .forms import ProfileForm
+
 
 # Create your views here.
 @login_required
 def update_profile(request):
     # we're going to get or create a profile from the db.
-    profile, created = Profile.objects.get_or_create(
-        user=request.user
-    )
+    profile, created = Profile.objects.get_or_create(user=request.user)
+    breakpoint()
     if request.method == "POST":
         form = ProfileForm(
             request.POST,
-            request.FILES, # the images that are passed back.
-            instance=profile
+            request.FILES,  # the images that are passed back.
+            instance=profile,
         )
         # check if the form is valid
         if form.is_valid():
             # save the form
             form.save()
-            return redirect('profile_edit')
+            return redirect("profile_edit")
             # redirect to "profile_edit"
     else:
         # render the form
         form = ProfileForm(instance=profile)
 
-    return render(
-        request,
-        "profiles/edit_profile.html",
-        { "form": form }
-    )
+    return render(request, "profiles/edit_profile.html", {"form": form})
