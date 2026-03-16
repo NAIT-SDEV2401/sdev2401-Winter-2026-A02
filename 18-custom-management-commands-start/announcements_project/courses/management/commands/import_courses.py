07b2afd4c5a3eba92e71ea90da79e07783765673
Daniel Mouris <dgmouris@gmail.com>
2026-03-16 13:43:55 -0600

    class progress pushed

--- 18-custom-management-commands-start/announcements_project/courses/management/commands/import_courses.py
+++ 18-custom-management-commands-start/announcements_project/courses/management/commands/import_courses.py
@@ -13,5 +13,16 @@ from courses.models import Course
 class Command(BaseCommand):
     help = "Import courses from a csv file."
 
+    # let's add an argument that will be the file path
+    # of the csv file.
+    def add_arguments(self, parser):
+        # add the csv argument
+        parser.add_argument(
+            "csv_file",  # the name of the argument
+            type=str,
+            help="csv file path to import.",
+        )
+
     def handle(self, *args, **kwargs):
-        print("hello import los csv.!")
+        print("hello import los csv!")
+        breakpoint()
