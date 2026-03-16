917b271b37c13fe2dd54b388bd0e6982e5d23d4a
Daniel Mouris <dgmouris@gmail.com>
2026-03-16 13:31:12 -0600

    class progress pushed

--- 18-custom-management-commands-start/announcements_project/courses/models.py
+++ 18-custom-management-commands-start/announcements_project/courses/models.py
@@ -6,6 +6,16 @@ from django.conf import settings
 from django.utils import timezone
 from datetime import datetime
 
+
+# let's create our new model for courses
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -20,22 +30,21 @@ class Assignment(models.Model):
 
     @classmethod
     def create_assignments_from_csv(cls, csv_file, owner):
-        decoded_file = csv_file.read().decode('utf-8').splitlines()
+        decoded_file = csv_file.read().decode("utf-8").splitlines()
         reader = csv.DictReader(decoded_file)
         assignments = []
         for row in reader:
             # Parse date and time
             naive_dt = datetime.strptime(
-                f"{row['date']} {row['time']}",
-                "%Y-%m-%d %H:%M"
+                f"{row['date']} {row['time']}", "%Y-%m-%d %H:%M"
             )
             dt = timezone.make_aware(naive_dt)
             # create assignment
             new_assignment, created = Assignment.objects.get_or_create(
-                title=row['title'],
-                description=row['description'],
+                title=row["title"],
+                description=row["description"],
                 due_date=dt,
-                owner=owner
+                owner=owner,
             )
             # keep track of created assignments
             assignments.append(new_assignment)
@@ -43,6 +52,7 @@ class Assignment(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Submission(models.Model):
     assignment = models.ForeignKey(
@@ -53,9 +63,8 @@ class Submission(models.Model):
 
     student_name = models.CharField(max_length=100)
 
-    file = models.FileField(upload_to='submissions/')
+    file = models.FileField(upload_to="submissions/")
     submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Submission by {self.student_name} for {self.assignment}"
-
