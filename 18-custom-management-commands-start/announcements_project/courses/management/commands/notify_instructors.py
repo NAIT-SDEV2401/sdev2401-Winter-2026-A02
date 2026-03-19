a44da628eb6c5c77ea6fea6866471fd051a4ce98
Daniel Mouris <dgmouris@gmail.com>
2026-03-19 08:51:05 -0600

    class progress pushed

--- 18-custom-management-commands-start/announcements_project/courses/management/commands/notify_instructors.py
+++ 18-custom-management-commands-start/announcements_project/courses/management/commands/notify_instructors.py
@@ -19,13 +19,24 @@ class Command(BaseCommand):
             instructor_notified=False,
         )
         # count them
-        new_submissions.count()
+        count = new_submissions.count()
         # loop through the submissions
         for submission in new_submissions:
             # get the instructor owner
             instructor = submission.assignment.owner
-            breakpoint()
             # send an email with send_mail from django
+            # we can send for each submission
+            # we can also send an email with all of the
+            # new submissions.
+            send_mail(
+                subject=f"New Submission Received for {submission.assignment.title}",
+                message=f"""
+                    New submission for {submission.assignment.title} from {submission.student_name}
+                    Download here {submission.file.url}
+                """,
+                from_email="notification@do-not-reply.com",
+                recipient_list=[instructor.email],
+            )
             # set the instrcutor_notified to true.
             # give a message, telling the user how many they've
             # sent.
