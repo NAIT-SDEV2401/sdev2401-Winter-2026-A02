012d766261add15d9bf57af1dfe2ce9bff3eebc4
Daniel Mouris <dgmouris@gmail.com>
2026-03-19 08:46:24 -0600

    class progress pushed

--- 18-custom-management-commands-start/announcements_project/courses/management/commands/notify_instructors.py
+++ 18-custom-management-commands-start/announcements_project/courses/management/commands/notify_instructors.py
@@ -1,13 +1,31 @@
-# I want a management command
-# help of "notify instructors about new submissions"
-# no arguments
+from django.core.management.base import BaseCommand
+from django.core.mail import send_mail
 
-# inside of the handle (what's executed)
-# fetch all submissions that have instructor_notified
-# of false
-# count them
-# loop through the submissions
-# send an email with send_mail from django
-# set the instrcutor_notified to true.
-# give a message, telling the user how many they've
-# sent.
+from courses.models import Submission
+
+
+# I want a management command
+class Command(BaseCommand):
+    # help of "notify instructors about new submissions"
+    help = "notify instructors about new submissions"
+
+    # no arguments
+
+    # inside of the handle (what's executed)
+    def handle(self, *args, **kwargs):
+        # fetch all submissions that have instructor_notified
+        # of false
+        new_submissions = Submission.objects.filter(
+            instructor_notified=False,
+        )
+        # count them
+        new_submissions.count()
+        # loop through the submissions
+        for submission in new_submissions:
+            # get the instructor owner
+            instructor = submission.assignment.owner
+            breakpoint()
+            # send an email with send_mail from django
+            # set the instrcutor_notified to true.
+            # give a message, telling the user how many they've
+            # sent.
