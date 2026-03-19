21b2e19898e65259ca76e73173efe4a465367a00
Daniel Mouris <dgmouris@gmail.com>
2026-03-19 09:30:53 -0600

    class progress pushed

--- 19-class-based-views-start/announcements_project/web/views.py
+++ 19-class-based-views-start/announcements_project/web/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 class HomePageView(View):
     template_name = "web/home.html"
+
+    # all class based view function
+    # get, post, patch, delete
+    # are going to take request as a param.
+    def get(self, request):
+        return render(
+            request,
+            self.template_name,
+        )
