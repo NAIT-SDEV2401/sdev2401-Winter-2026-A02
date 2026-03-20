f77ef18f10e729459c74702cbba9427667133a5f
Daniel Mouris <dgmouris@gmail.com>
2026-03-20 11:31:38 -0600

    class progress pushed

--- 19-class-based-views-start/announcements_project/courses/urls.py
+++ 19-class-based-views-start/announcements_project/courses/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
 
-from .views import bulk_assignment_upload, assignment_list, assignment_submission
+from .views import (
+    bulk_assignment_upload,
+    assignment_list,
+    assignment_submission,
+    AssignmentListView,
+)
 
 urlpatterns = [
-    path('bulk-assignment-upload/', bulk_assignment_upload, name='bulk_assignment_upload'),
-    path('assignments/', assignment_list, name='assignment_list'),
-    path('assignments/<int:assignment_id>/submit/', assignment_submission, name='assignment_submission'),
+    path(
+        "bulk-assignment-upload/", bulk_assignment_upload, name="bulk_assignment_upload"
+    ),
+    path(
+        "assignments/",
+        AssignmentListView.as_view(),
+        name="assignment_list",
+    ),
+    # path("assignments/", assignment_list, name="assignment_list"),
+    path(
+        "assignments/<int:assignment_id>/submit/",
+        assignment_submission,
+        name="assignment_submission",
+    ),
 ]
