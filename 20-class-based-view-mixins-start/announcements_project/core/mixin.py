ebdf0f29fc9fb4c32befc510b73aa9af56a64362
Daniel Mouris <dgmouris@gmail.com>
2026-03-23 13:32:42 -0600

    class progress pushed

---
+++ 20-class-based-view-mixins-start/announcements_project/core/mixin.py
@@ -0,0 +1,17 @@
+# we're going to be using
+# docs here https://docs.djangoproject.com/en/5.2/topics/auth/default/#django.contrib.auth.mixins.UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+
+# so this is going to be a class where we define the test
+class IsTeacherRoleMixin(UserPassesTestMixin):
+
+    # as per the docs we need to define the "test_func"
+    # which will check the permission on the user.
+    def test_func(self):
+        # this is going to be similar to the
+        # is_teacher function
+        # we're going to get the user on the self.request
+        # which is defined in our class.
+
+        return self.request.user.role == "Teacher"
