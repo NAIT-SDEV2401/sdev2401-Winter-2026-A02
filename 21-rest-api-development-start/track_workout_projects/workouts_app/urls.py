9ada020983d8491f4b03b921bac68b5cc61a51db
Daniel Mouris <dgmouris@gmail.com>
2026-03-26 09:12:23 -0600

    class progress pushed

--- 21-rest-api-development-start/track_workout_projects/workouts_app/urls.py
+++ 21-rest-api-development-start/track_workout_projects/workouts_app/urls.py
@@ -1,2 +1,13 @@
+from django.urls import path
+
 # just like we've done for the course
 # create a url mapping to our view.
+from .views import ExerciseAPIView
+
+urlpatterns = [
+    path(
+        "execise/",
+        ExerciseAPIView.as_view(),
+        name="exercise-api",
+    ),
+]
