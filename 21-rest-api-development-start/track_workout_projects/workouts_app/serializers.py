dd6a01b154df9fcdae6e39b530afcd9f8bdf5763
Daniel Mouris <dgmouris@gmail.com>
2026-03-26 08:48:22 -0600

    class progress pushed

---
+++ 21-rest-api-development-start/track_workout_projects/workouts_app/serializers.py
@@ -0,0 +1,21 @@
+# this file will be in all of your DRF apps
+# hold the serializers which
+# convert items to json or parse the json.
+
+from rest_framework import serializers
+
+from .models import Exercise
+
+
+# serializer the model of exercise
+class ExerciseSerializer(serializers.Serializer):
+    # id is on every model and is handled
+    # by the db, this is why it's read_only
+    id = serializers.IntegerField(read_only=True)
+    # fields
+    name = serializers.CharField(max_length=100)
+    # this is going to have the same restriction
+    # as the database model field
+    exercise_type = serializers.ChoiceField(
+        choices=Exercise.EXERCISE_TYPES,
+    )
