00d792ffb9005fa1538d76afec0deb33db0e07b3
Daniel Mouris <dgmouris@gmail.com>
2026-04-02 09:24:34 -0600

    class progress pushed

--- 22-rest-api-permissions-authentication-start/track_workout_projects/track_workout_projects/settings.py
+++ 22-rest-api-permissions-authentication-start/track_workout_projects/track_workout_projects/settings.py
@@ -132,9 +132,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Django REST framework settings
 REST_FRAMEWORK = {
+    # we're going to add our default authentication classes
+    "DEFAULT_AUTHENTICATEION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        # if there's not permissions
+    ],
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ]
+    ],
 }
